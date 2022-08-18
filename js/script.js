b6a45c89a3033c3cd0832a150e7c9487e58472a8
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-08-18 12:54:18 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,89 +1,212 @@
-var board = []; // variável do estado atual do jogo
-var currentGame = [1, 5, 11, 13, 15, 17];
-var savedGames = [];
-
-var state = { board: [], currentGame: [], savedGames: [] }; //Guardando as variáveis juntas
+var state = { currentGame: [], savedGames: [], board: [] };
 
 function start() {
-  addNumerToGame(50);
-  addNumerToGame(28);
-  addNumerToGame(29);
-  addNumerToGame(30);
-  addNumerToGame(31);
-  addNumerToGame(32);
-  saveGame();
-  removeNumberFromGame(32);
-  addNumerToGame(51);
-  saveGame();
-  console.log(state.savedGames);
- 
-  
-
-  
-}
-// Função adicionar número vai inserir o número passado como parâmetro na var currentGame. Como ele é uma lista, pode ser feito através do push
-function addNumerToGame(numberToAdd) {
-  //Se o número inserido for menor que 1 ou maior que 60, ignorar
-  if (numberToAdd < 1 || numberToAdd > 60) {
-    console.error("Número inválido", numberToAdd);
-    return;
-  }
-  //If para impedir uma quantidade maior do que seis números inseridos
-  if (state.currentGame.length >= 6) {
-    console.error("O jogo já está completo");
-    return;
-  }
-  // Se o resultado da função for verdadeiro
-  if (isNumberInGame(numberToAdd)) {
-    console.error(" Este número já está no jogo", numberToAdd);
-    return;
-  }
-  state.currentGame.push(numberToAdd);
+  createBoard();
+  newGame();
 }
 
-//Função para remover número
+function createBoard() {
+  state.board = [];
+
+  for (var i = 1; i <= 60; i++) {
+    state.board.push(i);
+  }
+}
+
+function newGame() {
+  state.currentGame = [];
+  render();
+}
+
+function render() {
+  renderBoard();
+  renderButtons();
+  renderSavedGames();
+}
+
+function renderBoard() {
+  var divGame = document.querySelector('#megasena-numbers');
+  divGame.innerHTML = '';
+
+  var ulGames = document.createElement('ul');
+  ulGames.classList.add('numbers');
+
+  for (var i = 0; i < state.board.length; i++) {
+    var currentNumber = state.board[i];
+
+    var liGame = document.createElement('li');
+    liGame.textContent = currentNumber.toString().padStart(2, '0');
+    liGame.classList.add('number');
+
+    liGame.addEventListener('click', handleNumberClick);
+
+    if (state.currentGame.includes(currentNumber)) {
+      liGame.classList.add('selected-number');
+    }
+
+    ulGames.appendChild(liGame);
+  }
+
+  divGame.appendChild(ulGames);
+}
+
+function handleNumberClick(event) {
+  var element = event.currentTarget;
+  var clickedNumber = Number(element.textContent);
+
+  var numberInGame = state.currentGame.includes(clickedNumber);
+  var totalNumbers = state.currentGame.length;
+
+  if (numberInGame) {
+    removeNumberFromGame(clickedNumber);
+  } else {
+    if (totalNumbers === 6) {
+      return;
+    } else {
+      addNumberToGame(clickedNumber);
+    }
+  }
+
+  render();
+}
+
 function removeNumberFromGame(numberToRemove) {
-  if (numberToRemove < 1 || numberToRemove > 60) {
-    console.error("Número inválido", numberToRemove);
-    return;
-  }
   var newGame = [];
+
   for (var i = 0; i < state.currentGame.length; i++) {
     var currentNumber = state.currentGame[i];
 
     if (currentNumber === numberToRemove) {
-      continue; // Quando achar o numero que o usuário quiser excluir, não faça nada, só continue. Porque quando o número não for o que ele quer excluir, adiciona em um novo jogo
-    } else {
-      newGame.push(currentNumber);
+      continue;
     }
-  } // Fim do loop
+
+    newGame.push(currentNumber);
+  }
+
   state.currentGame = newGame;
 }
-// Função para verificar se tem número repetido no jogo
-function isNumberInGame(numberToCheck) {
-  // Verifica se o numero inserido já está incluido na lista de currentGame, se sim, retorna verdadeiro, se não, retorna falso
 
-  /* 
-  Essa opção funciona, mas existe a opção abaixo que é mais prático 
-
-   if(state.currentGame.includes(numberToCheck)){
-     return true;
-     }
-    return false
-  */
-  return state.currentGame.includes(numberToCheck);
+function addNumberToGame(numberToAdd) {
+  state.currentGame.push(numberToAdd);
+  state.currentGame.sort();
 }
-function saveGame() {
-  if (!isGameComplete()) {
-    console.error(' O jogo não está completo');
-    return;
+
+function renderButtons() {
+  var divButtons = document.querySelector('#megasena-buttons');
+  divButtons.innerHTML = '';
+
+  var ulButtons = document.createElement('ul');
+  ulButtons.classList.add('buttons');
+
+  var liNewGameButton = renderNewGameButton();
+  var liRandomGameButton = renderRandomGameButton();
+  var liSaveGameButton = renderSaveGameButton();
+
+  ulButtons.appendChild(liNewGameButton);
+  ulButtons.appendChild(liRandomGameButton);
+  ulButtons.appendChild(liSaveGameButton);
+
+  divButtons.appendChild(ulButtons);
+}
+
+function renderNewGameButton() {
+  var li = document.createElement('li');
+  li.classList.add('button');
+
+  var button = document.createElement('button');
+
+  button.textContent = 'Novo jogo';
+  button.addEventListener('click', newGame);
+
+  li.appendChild(button);
+
+  return li;
+}
+
+function renderRandomGameButton() {
+  var li = document.createElement('li');
+  li.classList.add('button');
+
+  var button = document.createElement('button');
+
+  button.textContent = 'Jogo aleatório';
+  button.addEventListener('click', function () {
+    randomGame();
+  });
+
+  li.appendChild(button);
+
+  return li;
+}
+
+function renderSaveGameButton() {
+  var li = document.createElement('li');
+  li.classList.add('button');
+
+  var button = document.createElement('button');
+
+  button.textContent = 'Salvar jogo';
+  button.disabled = state.currentGame.length !== 6;
+
+  button.addEventListener('click', handleSaveGame);
+
+  li.appendChild(button);
+
+  return li;
+}
+
+function handleSaveGame() {
+  state.savedGames.push(state.currentGame);
+  newGame();
+}
+
+function renderSavedGames() {
+  var divSavedGames = document.querySelector('#megasena-saved-games');
+  divSavedGames.innerHTML = '';
+
+  if (state.savedGames.length === 0) {
+    divSavedGames.innerHTML = '<p>Nenhum jogo gravado até o momento.</p>';
   } else {
-    state.savedGames.push(state.currentGame);
+    var h2 = document.createElement('h2');
+    h2.textContent = 'Jogos salvos';
+
+    var ul = document.createElement('ul');
+    ul.classList.add('saved-games');
+
+    for (var i = 0; i < state.savedGames.length; i++) {
+      var currentGame = state.savedGames[i];
+
+      var li = document.createElement('li');
+
+      var numbers = currentGame
+        .map(number => number.toString().padStart(2, '0'))
+        .join(' ');
+
+      li.textContent = numbers;
+
+      ul.appendChild(li);
+    }
+
+    divSavedGames.appendChild(h2);
+    divSavedGames.appendChild(ul);
   }
- 
 }
-function isGameComplete() {
-  return state, currentGame.length === 6;
+
+function randomGame() {
+  state.currentGame = [];
+
+  while (state.currentGame.length < 6) {
+    var newNumber = Math.ceil(Math.random() * 60);
+    newNumber = Math.max(newNumber, 1);
+
+    if (!state.currentGame.includes(newNumber)) {
+      state.currentGame.push(newNumber);
+    }
+  }
+
+  state.currentGame.sort();
+
+  render();
 }
 
 start();
